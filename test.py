6d88f5adeddc8247bd9cff111c7f17364ac50b85
Irdina Nasir <93898599+irdinanasir@users.noreply.github.com>
2024-10-27 11:23:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ MUT_RATE = 0.2
 
 #TARGET: Our goal.
 TARGET = 'irdina'
+TARGET = st.text_input("Enter your name")
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyz'
