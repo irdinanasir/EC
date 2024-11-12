cf815ff7221aa62a4c290d7df7a05a784283e9e8
Irdina Nasir <93898599+irdinanasir@users.noreply.github.com>
2024-11-12 11:26:37 +0800

    Create es.py

---
+++ pages/es.py
@@ -0,0 +1,34 @@
+# ackley multimodal function
+from numpy import arange
+from numpy import exp
+from numpy import sqrt
+from numpy import cos
+from numpy import e
+from numpy import pi
+from numpy import meshgrid
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
+import streamlit as st
+
+st.title('evolutionary strategies')
+st.write('the surface plot of the ackley function showing the vast number of local optima')
+
+# objective function
+def objective(x, y):
+  return -20.0 * exp(-0.2 * sqrt(0.5 * (x**2 + y**2))) - exp(0.5 * (cos(2 * pi * x) + cos(2 * pi * y))) + e + 20
+
+# define range for input
+r_min, r_max = 10.0, 1.0
+# sample input range uniformly at 0.1 increments
+xaxis = arange(r_min, r_max, 0.1)
+yaxis = arange(r_min, r_max, 0.1)
+# create a mesh from the axis
+x, y = meshgrid(xaxis, yaxis)
+# compute targets
+results = objective(x, y)
+# create a surface plot with the jet color scheme
+figure = pyplot.figure()
+axis = figure.add_subplot(projection='3d')
+axis.plot_surface(x, y, results, cmap='jet')
+# show the plot
+st.pyplot(figure)
